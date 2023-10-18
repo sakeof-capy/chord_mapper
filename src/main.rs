2a25e6928513584602e06ef8a5b8180533a176ae
Fourier2718281828 <non-linear@ukr.net>
2023-10-18 17:16:56 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use rust_parser::list_parser;
+  
+  pub fn main() {
+      assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
+      println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+  }
