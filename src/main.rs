a60f92f211bf245a15ee792a96b0dbbecb4677b1
Fourier2718281828 <non-linear@ukr.net>
2023-11-08 12:06:05 +0200

    added: basic CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-use rust_parser_mine::list_parser;
-  
-  pub fn main() {
-      assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
-      println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
-  }
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Undefined command. Use 'chord_mapper help' for help information.");
+        return;
+    }
+
+    let command = &args[1];
+    match command.as_str() {
+        "parse" => {
+            if args.len() != 3 {
+                println!("Usage: chord_mapper parse <chord_notation>");
+            } else {
+                let chord_notation = &args[2];
+                println!(
+                    "The chord consists of the following notes: {}",
+                    chord_notation
+                );
+            }
+        }
+        "help" => {
+            println!("Chord Mapper - Help");
+            println!("Usage:");
+            println!("  chord_mapper parse <chord_notation> - Parse a chord notation");
+            println!("  chord_mapper help - See help information.");
+        }
+        _ => {
+            println!("Undefined command. Use 'chord_mapper help' for help information.");
+        }
+    }
+}
