a89b873e39015289597a91c7e62a860559e7e7c9
Fourier2718281828 <non-linear@ukr.net>
2023-11-08 12:05:38 +0200

    added: ChordParser struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
-peg::parser!{
-  pub grammar list_parser() for str {
-    pub rule number() -> u32
-      = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
+// use pest::Parser;
+use pest_derive::Parser;
 
-    pub rule list() -> Vec<u32>
-      = "[" l:(number() ** ",") "]" { l }
-  }
-}
+#[derive(Parser)]
+#[grammar = "grammars/chord.pest"]
+pub struct ChordParser;
+
+// pub fn parse_chord(input: &str) -> Result<(), pest::error::Error<Rule>> {
+//     let pairs = ChordParser::parse(Rule::root, input)?;
+//     println!("{:?}", pairs);
+//     // for pair in pairs {
+//     //     match pair.as_rule() {
+//     //         Rule::root => {
+//     //           println!("Found root: {}", pair.as_str());
+
+//     //         }
+//     //         Rule::chord => {
+//     //             println!("Found chord: {}", pair.as_str());
+//     //         }
+//     //         Rule::delimitor => {
+//     //             // Ignore delimiters.
+//     //         }
+//     //         _ => {
+//     //           eprintln!("Encountered an unexpected rule: {:?}", pair.as_rule());
+//     //       }
+//     //     }
+//     // }
+//     Ok(())
+// }
